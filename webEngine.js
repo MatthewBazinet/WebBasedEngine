6594888a582a86cb14bde3837d400f8cd76677aa
Nightrahkshi444 <nightrahkshi444@gmail.com>
2022-04-12 15:12:52 -0400

    made obj loader compatible with current rendering

--- webEngine.js
+++ webEngine.js
@@ -58,7 +58,8 @@ constructor(gl){
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+  //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(object.position), gl.STATIC_DRAW);
 
 // Now set up the colors for the faces. We'll use solid colors
   // for each face.
@@ -362,117 +363,10 @@ main();
 
 async function main()
 {
-
     const response = await fetch('https://webglfundamentals.org/webgl/resources/models/cube/cube.obj');  
     const text = await response.text();
 
-    //renderOBJ(text);
     InitWebGl(text);
-}
-
-function renderOBJ(object)
-{
-    document.addEventListener('keydown', onKeyDown, false);
-    const canvas = document.querySelector('#glcanvas');
-    const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-
-    // If we don't have a GL context, give up now
-
-    if (!gl) {
-        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
-        return;
-    }
-
-    const vs = `
-    attribute vec4 a_position;
-    attribute vec3 a_normal;
- 
-    uniform mat4 u_projection;
-    uniform mat4 u_view;
-    uniform mat4 u_world;
- 
-    varying vec3 v_normal;
- 
-    void main() {
-        gl_Position = u_projection * u_view * u_world * a_position;
-        v_normal = mat3(u_world) * a_normal;
-    }
-    `;
- 
-    const fs = `
-    precision mediump float;
- 
-    varying vec3 v_normal;
- 
-    uniform vec4 u_diffuse;
-    uniform vec3 u_lightDirection;
- 
-    void main () {
-        vec3 normal = normalize(v_normal);
-        float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
-        gl_FragColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
-    }
-    `;
-
-    const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
-    const data = parseOBJ(object);;
-
-    const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
-
-    const cameraTarget = [0, 0, 0];
-    const cameraPosition = [0, 0, 4];
-    const zNear = 0.1;
-    const zFar = 50;
- 
-  function degToRad(deg) {
-    return deg * Math.PI / 180;
-  }
- 
-  function render(time) {
-    time *= 0.001;  // convert to seconds
- 
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
- 
-    const fieldOfViewRadians = degToRad(60);
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
- 
-    const up = [0, 1, 0];
-    // Compute the camera's matrix using look at.
-    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
- 
-    // Make a view matrix from the camera matrix.
-    const view = m4.inverse(camera);
- 
-    const sharedUniforms = {
-      u_lightDirection: m4.normalize([-1, 3, 5]),
-      u_view: view,
-      u_projection: projection,
-    };
- 
-    gl.useProgram(meshProgramInfo.program);
- 
-    // calls gl.uniform
-    webglUtils.setUniforms(meshProgramInfo, sharedUniforms);
- 
-    // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
-    webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
- 
-    // calls gl.uniform
-    webglUtils.setUniforms(meshProgramInfo, {
-      u_world: m4.yRotation(time),
-      u_diffuse: [1, 0.7, 0.5, 1],
-    });
- 
-    // calls gl.drawArrays or gl.drawElements
-    webglUtils.drawBufferInfo(gl, bufferInfo);
- 
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
 }
 
 function InitWebGl(object) {
@@ -486,16 +380,18 @@ function InitWebGl(object) {
     return;
   }
 
+ const data = parseOBJ(object);
+
  camera.OnCreate(gl);
  cube.mesh = new Mesh(gl);
- cube.mesh.CreateBuffers(gl);
+ cube.mesh.CreateBuffers(gl, data);
 
  cube2.mesh = new Mesh(gl);
- cube2.mesh.CreateBuffers(gl);
+ cube2.mesh.CreateBuffers(gl, data);
 
 
     enemy1.mesh = new Mesh(gl);
-    enemy1.mesh.CreateBuffers(gl);
+    enemy1.mesh.CreateBuffers(gl, data);
     enemy1.OnCreate();
 
   // Vertex shader program
@@ -522,10 +418,10 @@ function InitWebGl(object) {
     }
   `;
 
-  const data = parseOBJ(object);
+  
 
   cube.mesh = new Mesh(gl);
-  cube.mesh.CreateBuffers(gl, object);
+  cube.mesh.CreateBuffers(gl, data);
 
   // Initialize a shader program; this is where all the lighting
   // for the vertices and so forth is established.
